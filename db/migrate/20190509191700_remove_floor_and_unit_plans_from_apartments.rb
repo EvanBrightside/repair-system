f07969a1dd7b3c3ce33e755a8e2fb2bed1dc80aa
EvanBrightside <xvanx84@gmail.com>
2019-05-11 00:49:06 +0300

    fix apt attrs

--- db/migrate/20190509191700_remove_floor_and_unit_plans_from_apartments.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFloorAndUnitPlansFromApartments < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :apartments, :floor_plan, :string
-    remove_column :apartments, :unit_plans, :json
-  end
-end
