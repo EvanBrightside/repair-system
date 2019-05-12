837b9334833a52aba7304f89dff068c719300aab
EvanBrightside <xvanx84@gmail.com>
2019-05-12 18:44:40 +0300

    fix deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,16 @@
-server 'repairsystem.ivanzabrodin.com', user: 'app', roles: %w[web app db]
-
+server '10.1.1.246', user: 'app', roles: %w[web app db]
 set :keep_releases, 3
 set :rails_env, :production
 set :branch, :master
-set :deploy_to, '/home/app/production'
+set :deploy_to, '/home/app/staging'
+set :gateway, 'bastion.molinos.ru -p 33112'
+set :user, 'app'
+
+# append :linked_files, '.env.staging.local'
+
+set :ssh_options,
+  user: fetch(:user),
+  forward_agent: false,
+  proxy: Net::SSH::Proxy::Command.new(
+    "ssh -l #{fetch(:user)} #{fetch(:gateway)} -W %h:%p"
+  )
